206431fd835126b50658185e9e8fe15b9e25a4c7
johnalpha <johnalpha@gmail.com>
2022-05-19 14:44:02 +0200

    CHORE: Fix linters and Add more logic to script.js

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ const navLinks = document.querySelectorAll('.mobile-nav-item');
 const welcome = document.querySelector('.welcome');
 
 function mobileMenu() {
-    hamburger.classList.toggle('active');
-    navMenu.classList.toggle('active');
-    welcome.classList.toggle('active');
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+  welcome.classList.toggle('active');
 }
 
 hamburger.addEventListener('click', mobileMenu);
@@ -15,68 +15,65 @@ hamburger.addEventListener('click', mobileMenu);
 const navLink = document.querySelectorAll('.nav-link');
 
 function closeMenu() {
-    hamburger.classList.remove('active');
-    navMenu.classList.remove('active');
-    welcome.classList.remove('active');
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+  welcome.classList.remove('active');
 }
 
 navLink.forEach((n) => n.addEventListener('click', closeMenu));
 
 function afterClick() {
-    navMenu.classList.remove('active');
-    hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+  hamburger.classList.remove('active');
 }
 
 navLinks.forEach((n) => n.addEventListener('click', afterClick));
 
-/* POPUP SCRIPT*/
+/* POPUP SCRIPT */
 
 /* Create dynamic projects section */
 
-
 /* POPUP WINDOW */
 
-
-const projects = [
-    {
-        name: 'Multi-Post Stories Gain+Glory',
-        image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
-        language: ['Ruby on Rails', 'css', 'JavaScript', 'html']
-    },
-    {
-        name: 'Multi-Post Stories Gain+Glory',
-        image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
-        language: ['Ruby on Rails', 'css', 'JavaScript', 'html']
-    },
-    {
-        name: 'Multi-Post Stories Gain+Glory',
-        image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
-        language: ['Ruby on Rails', 'css', 'JavaScript', 'html']
-    },
-    {
-        name: 'Multi-Post Stories Gain+Glory',
-        image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
-        language: ['Ruby on Rails', 'css', 'JavaScript', 'html']
-    },
-    {
-        name: 'Multi-Post Stories Gain+Glory',
-        image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
-        language: ['Ruby on Rails', 'css', 'JavaScript', 'html']
-    },
-    {
-        name: 'Multi-Post Stories Gain+Glory',
-        image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
-        language: ['Ruby on Rails', 'css', 'JavaScript', 'html']
-    }
-
-
+const projects = [{
+  name: 'Multi-Post Stories Gain+Glory',
+  image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+  language: ['Ruby on Rails', 'css', 'JavaScript', 'html'],
+},
+{
+  name: 'Multi-Post Stories Gain+Glory',
+  image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+  language: ['Ruby on Rails', 'css', 'JavaScript', 'html'],
+},
+{
+  name: 'Multi-Post Stories Gain+Glory',
+  image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+  language: ['Ruby on Rails', 'css', 'JavaScript', 'html'],
+},
+{
+  name: 'Multi-Post Stories Gain+Glory',
+  image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+  language: ['Ruby on Rails', 'css', 'JavaScript', 'html'],
+},
+{
+  name: 'Multi-Post Stories Gain+Glory',
+  image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+  language: ['Ruby on Rails', 'css', 'JavaScript', 'html'],
+},
+{
+  name: 'Multi-Post Stories Gain+Glory',
+  image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+  language: ['Ruby on Rails', 'css', 'JavaScript', 'html'],
+},
 ];
 const collection = document.getElementById('demo');
-for(let i =0; i < projects.length; i++) {
-    collection.innerHTML += `
+for (let i = 0; i < projects.length; i = 1 + i) {
+  collection.innerHTML += `
     <div class="collection-item">
                             <div class="collection-img">
-                                <img class="collection-img" src="${projects[i].image}" alt="project image">
+                                <img class="collection-img" src="${
+  projects[i].image
+}" alt="project image">
                             </div>
                             <div class="collection-info">
                                 <h4 class="project-name">
@@ -84,7 +81,11 @@ for(let i =0; i < projects.length; i++) {
                                 </h4>
                                 <div class="tags">
                                     
-                                    ${projects[i].language.map((l) => `<span class="tag">${l}</span>`).join('')}
+                                    ${projects[i].language
+    .map(
+      (l) => `<span class="tag">${l}</span>`,
+    )
+    .join('')}
 
                 
                                 </div>
@@ -102,14 +103,14 @@ const popupOverlay = document.querySelector('#popup-overlay');
 const closeButton = document.querySelector('#close-button');
 const btn = document.querySelectorAll('#popup-button');
 
-closeButton.addEventListener('click', ()=> {
-    popupWrapper.style.display = 'none';
-    popupOverlay.style.display = 'none';
+closeButton.addEventListener('click', () => {
+  popupWrapper.style.display = 'none';
+  popupOverlay.style.display = 'none';
 });
 
 btn.forEach((b) => {
-    b.addEventListener('click', () => {
-        popupWrapper.style.display = 'block';
-        popupOverlay.style.display = 'block';
-    });
+  b.addEventListener('click', () => {
+    popupWrapper.style.display = 'block';
+    popupOverlay.style.display = 'block';
+  });
 });
